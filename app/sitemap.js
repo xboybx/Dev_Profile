bff5dded894109173b7e3ec9df3bd1034e631395
username-xboybx <jeswanth012@gmail.com>
2026-02-20 00:24:43 +0530

    added sitemap and robots.txt

---
+++ app/sitemap.js
@@ -0,0 +1,16 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://www.jjaswanth.in',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 1,
+    },
+    // {
+    //   url: 'https://www.jjaswanth.in/about',
+    //   lastModified: new Date(),
+    //   changeFrequency: 'monthly',
+    //   priority: 0.8,
+    // },
+  ]
+}
